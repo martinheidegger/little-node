6c1671b2b8e786baf0660e632e9c22e8688678d6
Martin Heidegger <martin.heidegger@gmail.com>
2017-06-22 14:53:05 +0900

    CircleCI friendly entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-
-USER_ID="$(id -u node 2>/dev/null)"
-if [ ! $USER_ID ]; then
-  USER_ID=$(stat -c '%u' /usr/src/app)
-  USER_ID=${USER_ID:-9001}
-  echo "Creating the node user as ${USER_ID}"
-  adduser -s /bin/bash -u $USER_ID -D node
+USER_ID="$(id -u)"
+# Only try to use the node user if we have a /usr/src/app directory
+if [ -d /usr/src/app ]; then
+  USER_ID="$(id -u node 2>/dev/null)"
+  # Lets try to create a node user if it doesn't exist already
+  if [ -z "${USER_ID}" ]; then
+    USER_ID=$(stat -c '%u' /usr/src/app)
+    # In case /usr/src/app is mounted it will have a different user
+    # than the one logged in, lets use that userid for the new node user
+    if [ "${USER_ID}" != "$(id -u)" ]; then
+      USER_ID=${USER_ID:1004}
+      echo "Creating the node user with id: ${USER_ID}"
+      adduser -s /bin/bash -u $USER_ID -D node
+    fi
+  fi
 fi
-
-echo "Starting with UID: $USER_ID (node)"
 
 if [ -z "$@" ]; then
-  exec su-exec node /bin/bash
+  CMD=""
 else
-  exec su-exec node /bin/bash -c "$@"
+  CMD="-c \"$@\""
 fi
+
+CMD="/bin/bash ${CMD}"
+
+if [ "${USER_ID}" != "$(id -u)" ]; then
+  echo "Starting with UID: ${USER_ID} (node)"
+  CMD="su-exec node ${CMD}"
+fi
+
+${CMD}
+exit $?
