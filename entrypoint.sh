e36e57c53bee159f4b4492fe4e3228eecf0ebdb2
Martin Heidegger <martin.heidegger@gmail.com>
2017-06-22 12:54:12 +0900

    Creating a home because npm wants to write there.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! $USER_ID ]; then
   USER_ID=$(stat -c '%u' /usr/src/app)
   USER_ID=${USER_ID:-9001}
   echo "Creating the node user as ${USER_ID}"
-  adduser -s /bin/bash -u $USER_ID -D -H node
+  adduser -s /bin/bash -u $USER_ID -D node
 fi
 
 echo "Starting with UID: $USER_ID (node)"
